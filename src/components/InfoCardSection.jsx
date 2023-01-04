42897f1ce3f7d23f65bfc198d995ae40179ceaa3
Camille <camille.monis@gmail.com>
2023-01-05 06:49:30 +0800

    final link changes

--- src/components/InfoCardSection.jsx
+++ src/components/InfoCardSection.jsx
@@ -2,7 +2,7 @@ const InfoCardSection = ({imageName,title,body}) =>{
 
     return(
         <div className='flex-1 p-4'>
-            <img src={"easybank-website/"+imageName} alt="card-icon" className="pb-8 m-auto lg:m-0"/>
+            <img src={"/easybank-website"+imageName} alt="card-icon" className="pb-8 m-auto lg:m-0"/>
             <h3 className="text-lg font-light py-4">
                 {title}
             </h3>
